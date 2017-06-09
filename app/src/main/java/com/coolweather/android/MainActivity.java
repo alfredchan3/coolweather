a3f0edcdc5171cab2e43d00132aebb7e86cded0d
alfred <brilychen@gmail.com>
2017-06-09 20:20:52 +0800

    fix code

--- app/src/main/java/com/coolweather/android/MainActivity.java
+++ app/src/main/java/com/coolweather/android/MainActivity.java
@@ -2,15 +2,15 @@ package com.coolweather.android;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    protected void onCreate(Bundle savedIntanceState) {
+       super.onCreate(savedIntanceState);
         setContentView(R.layout.activity_main);
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
         if (prefs.getString("weather",null)!=null){
